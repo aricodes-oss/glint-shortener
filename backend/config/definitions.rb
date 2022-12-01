008ee2252523c5afb51ddfc0a7dad7bc1f355f02
Aria Taylor <ari@aricodes.net>
2022-12-01 16:06:32 -0500

    Initial commit

---
+++ backend/config/definitions.rb
@@ -0,0 +1,12 @@
+--2022-12-01 15:44:12--  https://gist.githubusercontent.com/castwide/28b349566a223dfb439a337aea29713e/raw/715473535f11cf3eeb9216d64d01feac2ea37ac0/rails.rb
+Loaded CA certificate '/etc/ssl/certs/ca-certificates.crt'
+Resolving gist.githubusercontent.com (gist.githubusercontent.com)... 185.199.109.133, 185.199.110.133, 185.199.111.133, ...
+Connecting to gist.githubusercontent.com (gist.githubusercontent.com)|185.199.109.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1135 (1.1K) [text/plain]
+Saving to: ‘rails.rb’
+
+     0K .                                                     100% 56.4M=0s
+
+2022-12-01 15:44:12 (56.4 MB/s) - ‘rails.rb’ saved [1135/1135]
+
